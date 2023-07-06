be2d029a762f9d9cd0fed5d64d6d8ba886aabeb6
AlexVlasenkov <pointgred63@gmail.com>
2023-07-06 13:02:47 +0300

    Added my func of visualize

--- preprocessingSignal.py
+++ preprocessingSignal.py
@@ -1,15 +1,18 @@
-import torch
-import numpy as np
-import os
+import csv
+import re
+
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import torch
 
 
 class Dataset():
     def __init__(self, root):
         self.root = root
+        self.dateline = self.read_data(self.root, 14, 1)
         self.dataset = self.build_dataset()
-        self.length = self.dataset.shape[1]
+        self.length = len(self.dataset)
         # self.minmax_normalize()
 
     def __len__(self):
@@ -31,37 +34,71 @@ class Dataset():
         except:
             return False
 
-    def build_dataset(self):
-        dataset = []
-        fname = 'data/lines-pos.csv'
-        skiprows = 15
-        nrows = 2
-        # for _file in os.listdir(self.root):
-        #   sample = np.loadtxt(os.path.join(self.root, _file)).T
-        #   dataset.append(sample)
+    def str2float(self, value):
+        matches = re.findall(r'\d+\.\d+', value)
+        res = float(matches[0])
 
-        for _file in os.listdir(self.root):
-            # 15 и 2 для Pos = 1
+        return res
+
+    def read_params(self):
+        with open(self.root) as csv_file:
+            csv_reader = csv.reader(csv_file)
+            rows = list(csv_reader)
+
+        x_resolution = self.str2float(rows[3].__getitem__(0))
+        y_resolution = self.str2float(rows[4].__getitem__(0))
+
+        x_calibration = self.str2float(rows[8].__getitem__(0))
+        y_calibration = self.str2float(rows[9].__getitem__(0))
+
+        return x_resolution, y_resolution, x_calibration, y_calibration
+
+    def read_data(self, fname, skiprows, nrows):
+        lst = []
+
+        with open(fname):
             data = pd.read_csv(fname, skiprows=skiprows, nrows=nrows)
             for elem in data:
                 if self.is_float(elem):
-                    dataset.append(float(elem))
+                    lst.append(float(elem))
             print(f'Num of row in {fname}: {skiprows + 1}')
-            print(dataset)
+            print(lst)
 
-        dataset = np.vstack(dataset).T
-        dataset = torch.from_numpy(dataset).float()
+        return lst
 
-        return dataset
+    def build_dataset(self):
+        ds = []
+        skiprows = 15
+        data = self.read_data(self.root, skiprows, 2)
+        for elem in data:
+            if self.is_float(elem):
+                ds.append(float(elem))
+        print(f'Num of row in {self.root}: {skiprows + 1}')
+        print(ds)
 
-    def minmax_normalize(self):
-        for index in range(self.length):
-            self.dataset[:, index] = (self.dataset[:, index] - self.dataset[:, index].min()) / (
-                    self.dataset[:, index].max() - self.dataset[:, index].min())
+        x_resolution, y_resolution, x_calibration, y_calibration = self.read_params()
+        scaled_positions = [position * x_resolution * x_calibration for position in ds]
+
+        return scaled_positions
+
+
+def minmax_normalize(self):
+    for index in range(self.length):
+        self.dataset[:, index] = (self.dataset[:, index] - self.dataset[:, index].min()) / (
+                self.dataset[:, index].max() - self.dataset[:, index].min())
 
 
 if __name__ == '__main__':
     # ./data
-    dataset = Dataset(r'data')
-    plt.plot(dataset.dataset[:, 0].T)
+    dataset = Dataset(r'data/lines-pos.csv')
+    # plt.plot(dataset.dataset[:, 0].T)
+    plt.title('Signals')
+    plt.xlabel('Dateline, um')
+    plt.ylabel('Positions, um')
+    xi = list(range(len(dataset.dateline)))
+    plt.xlim(0, 640)
+    plt.plot(dataset.dataset)
+    plt.xticks(np.arange(min(dataset.dateline), max(dataset.dateline) + 1, 20))
+    plt.xticks(rotation=90)
     plt.show()
+    plt.close()
